f0f28f7fc0677d5e5366bd29a13691ca965b9d46
Stephanie Kwak <stephanieyoungkwak@gmail.com>
2016-02-04 20:06:13 -0800

    finish regex golf

--- regex.js
+++ regex.js
@@ -10,32 +10,26 @@ function popProp(str) {
 }
 
 // 3. ferret, ferry, and ferrari
-
 function ferretFerryFerrari(str) {
-  // return /ferret/.test(str) && /ferry/.test(str) && /;
+  return /ferr(e|y|a)t?(ri)?/.test(str);
 }
 
 // 4. any word ending in ious
+function ious(str) {
+  return /.(ious)/.test(str);
+}
 
 // 5. any whitespace character followed by a dot, comma, colon, or semicolon
+function whiteSpacePunctuation(str) {
+  return /\s(\.|,|;)/.test(str);
+}
 
 // 6. any word longer than six letters
+function longerThanSixLetters(str) {
+  return /\w{7,}/.test(str);
+}
 
 // 7. any word without the letter e
-
-// car cat test
-var testString = "car1";
-var testString2 = "carcat";
-var testString3 = "cat1231231312309";
-
-console.log(carCat(testString));
-console.log(carCat(testString2));
-console.log(carCat(testString3));
-
-var testString4 = "popprop";
-var testString5 = "pooooprop";
-var testString6 = "pop";
-
-console.log(popProp(testString4));
-console.log(popProp(testString5));
-console.log(popProp(testString6));
+function noLetterE(str) {
+  return /\b[^e\W]+\b/.test(str);
+}
